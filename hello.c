a885d05ee92ae1ba9fd2a4b0976febbc26beccce
asvaaron <asvaaaron@gmail.com>
2021-02-21 18:05:59 -0600

    Add C files and headers
    Add hello.c file as example
    Add hello.h header as an example of ussage
    Validate arguments received in the main.c file

---
+++ hello.c
@@ -0,0 +1,10 @@
+//
+// Created by aaron on 21/2/21.
+//
+
+#include "include/hello.h"
+
+void myPrintHelloMake(void) {
+
+    printf("Hello makefiles!\n");
+}
---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "include/hello.h"
+
+int main(int argc, char **argv) {
+    printf("You have entered %d\n", argc - 1);
+
+    if ((argc - 1) != 2) {
+        printf("Not enough cars both west and east \n");
+    } else {
+        // Arguments starts at 1
+        int cars_west = atoi(argv[1]);
+        int cars_east = atoi(argv[2]);
+
+        printf("Cars West: %d, Cars East: %d \n", cars_east, cars_west);
+
+
+        myPrintHelloMake();
+
+    }
+
+
+    return 0;
+}

4273fb63ab95d2a5d4ed088da2c529c7b841cedc
asvaaron <asvaaaron@gmail.com>
2021-02-21 18:58:18 -0600

    Add struct for the car type
    Create struct for the car, define directions

---
+++ include/car.h
@@ -0,0 +1,17 @@
+//
+// Created by aaron on 21/2/21.
+//
+
+#ifndef SOA_TAREA1_CAR_H
+#define SOA_TAREA1_CAR_H
+
+// Define directions
+#define LEFT_DIRECTION "L"
+#define RIGHT_DIRECTION "R"
+
+struct Car{
+    char * name;
+    char * direction;
+};
+
+#endif //SOA_TAREA1_CAR_H

eb2d1a10bbdde7f6a1a12d8459421f28fb764beb
asvaaron <asvaaaron@gmail.com>
2021-02-27 05:21:00 -0600

    Define bridge structure
    Add function for printing car parts

--- include/car.h
+++ include/car.h
@@ -6,8 +6,8 @@
 #define SOA_TAREA1_CAR_H
 
 // Define directions
-#define LEFT_DIRECTION "L"
-#define RIGHT_DIRECTION "R"
+#define LEFT_DIRECTION -1
+#define RIGHT_DIRECTION 1
 #define TRUE 1
 #define FALSE 0
 #define BOOL unsigned int
@@ -21,15 +21,22 @@ struct Car{
     int dir; // -1 left, 1 right
 };
 
+struct Bridge{
+    int left_index;
+    int right_index;
+    int dir;
+};
+
 struct Road{
     int left_index;
     BOOL bridge; // Boolean flag
     int right_index;
     int cars_total;
     int * road;
+    struct Bridge * main_bridge;
 };
 
-
+void print_card(struct Car car);
 
 
 #endif //SOA_TAREA1_CAR_H
--- src/car.c
+++ src/car.c
@@ -4,6 +4,10 @@
 
 #include "../include/car.h"
 
-
+void print_card(struct Car car){
+    printf("Car name %d\n" , car.car_name);
+    printf("Car pos %d\n" , car.pos);
+    printf("Car direction %d\n" , car.dir);
+}
 
 

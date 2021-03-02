d4d52bde83622f493c64a4ec9d7c60fad311453e
asvaaron <asvaaaron@gmail.com>
2021-03-02 13:48:01 -0600

    Add color header

---
+++ include/color.h
@@ -0,0 +1,31 @@
+//
+// Created by aaron on 28/2/21.
+//
+
+#ifndef SOA_TAREA1_COLOR_H_H
+#define SOA_TAREA1_COLOR_H_H
+
+#include <stdio.h>
+#include "color.h"
+
+//Change color text red
+void red_print();
+
+//Change color text yellow
+void yellow_print();
+
+//Change color text green
+void green_print();
+
+//Change color text blue
+void blue_print();
+
+//Change color text blue
+void purple_print();
+
+void reset_color_print();
+
+
+
+
+#endif //SOA_TAREA1_COLOR_H_H
